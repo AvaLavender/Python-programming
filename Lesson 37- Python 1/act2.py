5bc8a961dcc187061bd3e063032d38613f8d5883
AvaLavender <aavanib130313@gmail.com>
2026-04-24 12:11:30 +0400

    intial comment

---
+++ Lesson 37- Python 1/act2.py
@@ -0,0 +1,10 @@
+#print a sentence
+print("Welcome to the world of programming!")
+
+print(4) 
+
+print("Hai python! \n haii")
+
+print("Keywords in python are:")
+import keyword
+print(keyword.kwlist)
