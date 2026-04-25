ce3aca6c58b85572069b7d89ad6ac05387a06d26
AvaLavender <aavanib130313@gmail.com>
2026-04-25 16:33:01 +0400

    initial comment

---
+++ Lesson 38 python 2/python ops/python ops.py
@@ -0,0 +1,22 @@
+num1 = 3
+num2 = 7
+
+print("Number 1", num1)
+print("Number 2", num2)
+
+print("Addition", num1 + num2)
+print("Subtraction", num1 - num2)
+print("Multiplication", num1 * num2)
+print("Division", num1 / num2)
+print("Floor Division", num1 // num2)
+print("Modulus", num1 % num2)
+print("Square", num2 ** 2)
+print("Square root", num1 ** 0.5)
+
+print("Equal?", num1 == num2)
+print("Not Equal?", num1 != num2)
+print("NUmber 1 greater?", num1>num2)
+print("NUmber 2 greater?", num1<num2)
+
+result = num1/2 + num2**2 + 10
+print("Result of equation is:", result)
