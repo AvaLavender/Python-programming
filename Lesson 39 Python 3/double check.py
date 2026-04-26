a13aeaff490cf23ce3ff7ee7619647fa431df9a1
AvaLavender <aavanib130313@gmail.com>
2026-04-26 11:58:42 +0400

    initial comment

---
+++ Lesson 39 Python 3/double check.py
@@ -0,0 +1,12 @@
+num = int(input("Enter number:"))
+
+if num> 50:
+    print("number is greater than 50")
+    if num%2==0:
+        print("it is also even")
+
+    else: 
+        print("it is also odd")
+
+else:
+    print("number is less than 50")
