cf825e8fb518f0d85c965be09cf41a8bddaf4dbd
AvaLavender <aavanib130313@gmail.com>
2026-04-30 21:45:36 +0530

    main

---
+++ Lesson 43 python 7/no guess.py
@@ -0,0 +1,38 @@
+import random
+x = random.randint(1, 100)
+i = 5
+
+y = int(input("Enter number to guess: "))
+
+while y != x:
+
+    if y < x:
+        print('Oops, thats wrong. Try higher')
+    if y > x:
+        print("Oops, thats not right. Try lower")
+    
+    i = (i - 1)
+
+    if i==0:
+        print("Sorry, your chances are over. The number was: ", x)
+        break 
+    y = int(input("Enter number to guess: "))
+
+
+if y == x :
+    print('Yay, you are correct!')
+
+
+ git config --global user.name "AvaLavender"
+$ git config --global user.email aavanib130313@gmail.com
+
+
+
+
+
+    
+
+         
+
+
+
